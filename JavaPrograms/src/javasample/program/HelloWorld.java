be8a67298351f7487d65e414aae990079a480eed
Pallavi-BA <pallavibapallavi@gmail.com>
2024-07-08 12:39:10 +0530

    First Commit

---
+++ JavaPrograms/src/javasample/program/HelloWorld.java
@@ -0,0 +1,15 @@
+package javasample.program;
+
+public class HelloWorld {
+	public void display()
+	{
+		System.out.println (" HELLO WORLD ");
+	}
+		
+	public static void main(String[] args) {
+		HelloWorld obj1 = new HelloWorld();
+		obj1.display();
+	}
+		// TODO Auto-generated method stub
+
+	}
---
+++ JavaPrograms/src/javasample/program/SumofTwoNumbers.java
@@ -0,0 +1,18 @@
+package javasample.program;
+
+public class SumofTwoNumbers {
+
+	public void display(int n1, int n2)
+	{
+		int sum= n1+n2;
+		System.out.println("Sum is : "+sum); 
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SumofTwoNumbers  obj1 = new SumofTwoNumbers ();
+		obj1.display(99, 999);
+
+	}
+
+}
